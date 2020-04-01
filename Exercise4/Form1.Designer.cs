645c4d0e55233fcfc3d8c3ba525fd1ff57381a43
Joey <gallottaj@hotmail.com>
2020-04-01 15:00:37 -0700

    Add project files.

---
+++ Exercise4/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Exercise4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userInputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.userOutputLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userInputTextBox
+            // 
+            this.userInputTextBox.Location = new System.Drawing.Point(227, 134);
+            this.userInputTextBox.Name = "userInputTextBox";
+            this.userInputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userInputTextBox.TabIndex = 0;
+            this.userInputTextBox.TextChanged += new System.EventHandler(this.userInputTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter a Number in Seconds:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // userOutputLabel
+            // 
+            this.userOutputLabel.AutoSize = true;
+            this.userOutputLabel.Location = new System.Drawing.Point(227, 185);
+            this.userOutputLabel.Name = "userOutputLabel";
+            this.userOutputLabel.Size = new System.Drawing.Size(0, 13);
+            this.userOutputLabel.TabIndex = 3;
+            this.userOutputLabel.Click += new System.EventHandler(this.userOutputLabel_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(230, 223);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "QUIT";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 340);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.userOutputLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userInputTextBox);
+            this.Name = "Form1";
+            this.Text = "Time Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userInputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label userOutputLabel;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Exercise4/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userInputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal userInput;
+                userInput = decimal.Parse(userInputTextBox.Text);
+                decimal seconds_to_minutes;
+                decimal seconds_to_hours;
+                decimal seconds_to_days;
+                {
+                    //minutes
+                    if (userInput >= 60)
+                    {
+                        seconds_to_minutes = userInput / 60;
+                        userOutputLabel.Text = seconds_to_minutes.ToString("") + " " + "minute(s)";
+                    }
+                    //hours
+                    if (userInput >= 3600)
+                    {
+                        seconds_to_hours = userInput / 3600;
+                        userOutputLabel.Text = seconds_to_hours.ToString("") + " " + "hour(s)";
+                    }
+                    //days
+                    if (userInput >= 86400)
+                    {
+                        seconds_to_days = userInput / 86400;
+                        userOutputLabel.Text = seconds_to_days.ToString("") + " " + "day(s)";
+                    }
+                    if (userInput < 60)
+                    {
+                        userOutputLabel.Text = userInput.ToString() + " " + "second(s)";
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+     
+        }
+
+
+
+        private void userOutputLabel_Click(object sender, EventArgs e)
+        {
+            userOutputLabel.Text = "";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
